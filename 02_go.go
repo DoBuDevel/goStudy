d050ce49861e01165859c78d0b5d1a3590c0ad62
cmc <chhappy05@gmail.com>
2017-07-22 10:26:36 +0900

    test1

---
+++ 02_go.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+    ival = 0
+}
+
+func zeroptr(iptr *int) {
+    *iptr = 0
+}
+
+func main() {
+    i := 100
+    fmt.Println("초기 값 : ", i)
+
+    zeroval(i)
+    fmt.Println("zeroval : ", i)
+
+    fmt.Println("%i : ", &i)
+
+    zeroptr(&i)
+    fmt.Println("zeroptr : ", i)
+
+    fmt.Println("포인터 :", &i)
+
+    fmt.Println("포인터 :", i)
+    //fmt.Println(*i)
+}
